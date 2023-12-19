25c6e51b4bb26b5e3866c5f6d40650e5df68754b
MikeJoester <dan.nguyen200206@vnuk.edu.vn>
2023-12-19 12:54:14 +0700

    issue #3: init livewire modules

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    @livewire('category-list')
+                    @livewire('categories')
                 </div>
             </div>
         </div>
